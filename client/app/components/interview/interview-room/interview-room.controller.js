89c66db0d185fb4955538132e40a5a6a9fb18b85
ok2ju <soulSpringg@gmail.com>
2016-04-14 15:46:28 +0300

    messages when no interviews

--- client/app/components/interview/interview-room/interview-room.controller.js
+++ client/app/components/interview/interview-room/interview-room.controller.js
@@ -4,10 +4,9 @@ function InterviewRoomController(Vendor, myself, iOwnerInterviews, iCandidateInt
 
   vm.user = myself;
   vm.ownerInterviews = iOwnerInterviews;
-  vm.candidateInterviews = vm.editedInterviews;
   vm.openConferenceRoom = openRequestedPopupService.openRequestedPopup;
 
-  vm.editedInterviews = iCandidateInterviews.map((interview) => {
+  vm.candidateInterviews = iCandidateInterviews.map((interview) => {
     const message = ['Interview with', interview.company.name];
     interview.title = message.join(' ');
     return interview;
@@ -15,13 +14,13 @@ function InterviewRoomController(Vendor, myself, iOwnerInterviews, iCandidateInt
 
   let mergedInterviews = vm.ownerInterviews.concat(vm.candidateInterviews);
   
-  /*vm.completedInterviews = mergedInterviews.filter((interview) => {
+  vm.completedInterviews = mergedInterviews.filter((interview) => {
     return interview.status === 'Completed';
   });
 
   vm.inprogressInterviews = mergedInterviews.filter((interview) => {
     return interview.status === 'InProgress';
-  });*/
+  });
 }
 
 export default InterviewRoomController;
